ca41172002029ba4858e01a7541fdc154a0230c2
Sergey Ponomarev <me@sergey-ponomarev.ru>
2022-06-06 11:41:21 +0300

    Refactor `ChainlinkOracle.getRoundData` tests

--- test/chainlink_oracle.js
+++ test/chainlink_oracle.js
@@ -1,24 +1,27 @@
 const ChainlinkOracle = artifacts.require("ChainlinkOracle");
 
-contract("ChainlinkOracle", function () {
+contract("ChainlinkOracle", () => {
   let oracle
 
-  beforeEach(async function () {
+  beforeEach(async () => {
     oracle = await ChainlinkOracle.deployed();
   });
 
   describe(".getRoundData", function () {
-  /*
-    Raw Transmission data sample:
+    /*
+      Raw Transmission data sample:
 
-    slot: u64 = 138620452,
-    timestamp: u32 = 1654260800,
-    _padding0: u32 = 0,
-    answer: i128 = 176139103829,
-    _padding1: u64 = 0,
-    _padding2: u64 = 0,
-  */
-    const transmission = "0x242e43080000000040049a6200000000556eb50229000000000000000000000000000000000000000000000000000000"
+      slot: u64 = 138620452,
+      timestamp: u32 = 1654260800,
+      _padding0: u32 = 0,
+      answer: i128 = 176139103829,
+      _padding1: u64 = 0,
+      _padding2: u64 = 0,
+    */
+    const transmission = "0x" +
+      "242e43080000000040049a6200000000" +
+      "556eb502290000000000000000000000" +
+      "00000000000000000000000000000000"
 
     it('should extract timestamp of a round from transmission struct data', async () => {
       let { timestamp } = await oracle.getRoundData(transmission);
