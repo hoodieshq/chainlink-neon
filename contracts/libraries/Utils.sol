23b3c6ed8c5a0eadecb92a708c4d0d07d0f49be8
Sergey Ponomarev <me@sergey-ponomarev.ru>
2022-06-08 18:39:44 +0300

    Extract helper functions into the `Utils` library

---
+++ contracts/libraries/Utils.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "solidity-bytes-utils/contracts/BytesLib.sol";
+
+library Utils {
+    using BytesLib for bytes;
+
+    struct Round {
+        uint80 roundId;
+        int128 answer;
+        uint32 timestamp;
+    }
+
+    struct Header {
+        uint8 decimals;
+        string description;
+        uint256 version;
+    }
+
+    /*
+        pub struct Transmission {
+            pub slot: u64,
+            pub timestamp: u32,
+            pub _padding0: u32,
+            pub answer: i128,
+            pub _padding1: u64,
+            pub _padding2: u64,
+        }
+    */
+    uint8 private constant transmissionTimestampOffset = 8;  // slot:8
+    uint8 private constant transmissionAnswerOffset = 16;    // slot:8 + timestamp:4 + _padding0:4
+
+    /*
+        pub struct Transmissions {
+            pub version: u8,
+            pub state: u8,
+            pub owner: Pubkey,
+            pub proposed_owner: Pubkey,
+            pub writer: Pubkey,
+            /// Raw UTF-8 byte string
+            pub description: [u8; 32],
+            pub decimals: u8,
+            pub flagging_threshold: u32,
+            pub latest_round_id: u32,
+            pub granularity: u8,
+            pub live_length: u32,
+            live_cursor: u32,
+            historical_cursor: u32,
+        }
+    */
+    uint8 private constant headerVersionOffset = 0;
+    uint8 private constant headerDescriptionOffset = 98;    // version:1 + state:1 + owner:32 + proposed_owner:32 + writer:32
+    uint8 private constant headerDescriptionLength = 32;
+    uint8 private constant headerDecimalsOffset = 130;      // version:1 + state:1 + owner:32 + proposed_owner:32 + writer:32 + description:32
+
+    // Data extraction helpers
+
+    function extractRound(uint80 roundId, bytes memory rawTransmission) public pure returns (Round memory) {
+        uint32 timestamp = readLittleEndianUnsigned32(rawTransmission.toUint32(transmissionTimestampOffset));
+        int128 answer = readLittleEndianSigned128(rawTransmission.toUint128(transmissionAnswerOffset));
+        return Round(roundId, answer, timestamp);
+    }
+
+    function extractHeader(bytes memory rawTransmissions) public pure returns (Header memory) {
+        return Header(
+            rawTransmissions.toUint8(headerDecimalsOffset),
+            bytesToString(rawTransmissions.slice(headerDescriptionOffset,headerDescriptionLength)),
+            rawTransmissions.toUint8(headerVersionOffset)
+        );
+    }
+
+    function bytesToString(bytes memory _bytes) private pure returns (string memory) {
+        uint8 length = 0;
+        while(_bytes[length] != 0) {
+            length++;
+        }
+
+        bytes memory bytesArray = new bytes(length);
+        for (uint8 i = 0; i < length; i++) {
+            bytesArray[i] = _bytes[i];
+        }
+
+        return string(bytesArray);
+    }
+
+    // Little endian helpers
+
+    function readLittleEndianUnsigned32(uint32 input) private pure returns (uint32) {
+        input = ((input & 0xFF00FF00) >> 8) | ((input & 0x00FF00FF) << 8);
+        return uint32(input << 16) | (input >> 16);
+    }
+
+    function readLittleEndianSigned128(uint128 input) private pure returns (int128) {
+        input = ((input << 8) & 0xFF00FF00FF00FF00FF00FF00FF00FF00) | ((input >> 8) & 0x00FF00FF00FF00FF00FF00FF00FF00FF);
+        input = ((input << 16) & 0xFFFF0000FFFF0000FFFF0000FFFF0000) | ((input >> 16) & 0x0000FFFF0000FFFF0000FFFF0000FFFF);
+        input = ((input << 32) & 0xFFFFFFFF00000000FFFFFFFF00000000) | ((input >> 32) & 0x00000000FFFFFFFF00000000FFFFFFFF);
+        return int128((input << 64) | ((input >> 64) & 0xFFFFFFFFFFFFFFFF));
+    }
+}
--- test/chainlink_oracle.js
+++ test/chainlink_oracle.js
@@ -1,104 +1,10 @@
 const ChainlinkOracle = artifacts.require("ChainlinkOracle");
 
 contract("ChainlinkOracle", () => {
-  let oracle;
-
-  beforeEach(async () => {
-    oracle = await ChainlinkOracle.deployed();
-  });
-
   it('should set feed address from the contract', async () => {
     contract = await ChainlinkOracle.new("0xb22f4bfe7b663a29da31c40b32ab0b6f96c8ab1946c517b2c056710a352719ad")
 
     let feedAddress = await contract.feedAddress();
     assert.equal(feedAddress, "0xb22f4bfe7b663a29da31c40b32ab0b6f96c8ab1946c517b2c056710a352719ad")
-  });
-
-  describe(".extractRound", function () {
-    /*
-      Raw Transmission data sample:
-
-      slot: u64 = 138620452,
-      timestamp: u32 = 1654260800,
-      _padding0: u32 = 0,
-      answer: i128 = 176139103829,
-      _padding1: u64 = 0,
-      _padding2: u64 = 0,
-    */
-    const transmission = "0x" +
-      "242e43080000000040049a6200000000" +
-      "556eb502290000000000000000000000" +
-      "00000000000000000000000000000000";
-
-    const roundIdInput = 31337;
-
-    it('should set round id', async () => {
-      let { roundId } = await oracle.extractRound(roundIdInput, transmission);
-
-      assert.equal(roundId, 31337);
-    });
-
-    it('should extract timestamp of a round from transmission struct data', async () => {
-      let { timestamp } = await oracle.extractRound(roundIdInput, transmission);
-
-      assert.equal(timestamp, 1654260800);
-    });
-
-    it('should extract answer of a round from transmission struct data', async () => {
-      let { answer } = await oracle.extractRound(roundIdInput, transmission);
-
-      assert.equal(answer, 176139103829);
-    });
-  });
-
-  describe(".extractRound", function () {
-    /*
-      Raw Header data sample:
-
-      version: u8 = 2
-      state: u8 = 1
-      owner: Pubkey
-      proposed_owner: Pubkey
-      writer: Pubkey
-      description: [u8; 32] = "ETH / USD"
-      decimals: u8 = 8
-      flagging_threshold: u32
-      latest_round_id: u32 = 1638131
-      granularity: u8 = 30
-      live_length: u32 = 1024
-      live_cursor: u32 = 755
-      historical_cursor: u32 = 54604
-    */
-    const header = "0x" +
-      "020111d3be3f3544f970bd6fd0d49cc6" +
-      "9cd3ea549b220b34c874ede871afe057" +
-      "550f0000000000000000000000000000" +
-      "00000000000000000000000000000000" +
-      "00006c6670e4187ad830d9c44710b498" +
-      "044edee2e3d2c1512d030b73cebdc092" +
-      "fc3e455448202f205553440000000000" +
-      "00000000000000000000000000000000" +
-      "00000800000000f3fe18001e00040000" +
-      "f30200004cd500000000000000000000" +
-      "00000000000000000000000000000000" +
-      "00000000000000000000000000000000"
-
-    it('should extract version of a feed from header struct data', async () => {
-      let { version } = await oracle.extractHeader(header);
-
-      assert.equal(version, 2);
-    });
-
-    it('should extract description of a feed from header struct data', async () => {
-      let { description } = await oracle.extractHeader(header);
-
-      assert.equal(description, "ETH / USD");
-    });
-
-    it('should extract decimals of a feed from header struct data', async () => {
-      let { decimals } = await oracle.extractHeader(header);
-
-      assert.equal(decimals, 8);
-    });
   });
 });
