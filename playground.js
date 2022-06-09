05a7e9251db5aeaf6881c10de6b088ced632434c
Sergey Ponomarev <me@sergey-ponomarev.ru>
2022-06-09 12:26:11 +0300

    Fix playground's env variable

--- playground.js
+++ playground.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const web3 = new Web3(new Web3.providers.HttpProvider("https://proxy.devnet.neonlabs.org/solana"));
 const aggregatorV3InterfaceABI = JSON.parse(fs.readFileSync("./AggregatorV3Interface.json"));
 
-const contract = new web3.eth.Contract(aggregatorV3InterfaceABI, process.env.FEED_ADDRESS)
+const contract = new web3.eth.Contract(aggregatorV3InterfaceABI, process.env.ORACLE_ADDRESS)
 
 contract.methods.version().call().then((version) => { console.log("Version:", version) })
 contract.methods.description().call().then((description) => { console.log("description:", description) })
