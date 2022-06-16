b7194c22b3fa3605e1d92bc54abe8ee0f031add8
Sergey Ponomarev <me@sergey-ponomarev.ru>
2022-06-16 15:11:58 +0300

    Remove ChainlinkOracle tests-specific hacks (#10)

--- migrations/1654258882_chainlink_oracle.js
+++ migrations/1654258882_chainlink_oracle.js
@@ -4,7 +4,9 @@ const Utils = artifacts.require("Utils");
 const feedAddress = process.env.FEED_ADDRESS || "0xb22f4bfe7b663a29da31c40b32ab0b6f96c8ab1946c517b2c056710a352719ad"
 
 module.exports = function(deployer, network) {
+  if (network === 'test') return
+
   deployer.deploy(Utils);
   deployer.link(Utils, ChainlinkOracle);
-  deployer.deploy(ChainlinkOracle, feedAddress, network !== 'test');
+  deployer.deploy(ChainlinkOracle, feedAddress);
 };
