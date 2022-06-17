2ff69dd589707dfaf56f6948c8d53e2d79aac5dc
Sergey Ponomarev <me@sergey-ponomarev.ru>
2022-06-17 14:24:16 +0300

    Fix migrations in tests (#13)

--- migrations/1654258882_chainlink_oracle.js
+++ migrations/1654258882_chainlink_oracle.js
@@ -1,10 +1,10 @@
 const ChainlinkOracle = artifacts.require("ChainlinkOracle");
 const Utils = artifacts.require("Utils");
 
-if (!process.env.FEED_ADDRESS) throw(new Error('FEED_ADDRESS is not set'));
 
 module.exports = function(deployer, network) {
   if (network === 'test') return
+  if (!process.env.FEED_ADDRESS) throw(new Error('FEED_ADDRESS is not set'));
 
   deployer.deploy(Utils);
   deployer.link(Utils, ChainlinkOracle);
